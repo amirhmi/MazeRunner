231de8dd3dfa4e0a05fbb9eaa1dc5ce51a733ef2
amirhmi <amirhmi1377@gmail.com>
2019-05-24 15:34:49 +0430

    go forward until 10cm of object

--- ultrasonic/ultrasonic.ino
+++ ultrasonic/ultrasonic.ino
@@ -1,6 +1,6 @@
 #include <hcsr04.h>
 #include <Stepper.h>
-
+#include <Time.h>
 
 // Stepper motor defenitions and constructor 
 #define STEPS 200
@@ -8,9 +8,9 @@
 #define ONE_CM (ONE_REV / 18.2)
 #define WHEELS_DIST (16.7)
 
-#define LEFT_FRONT (1)
+#define LEFT_FORWARD (1)
 #define LEFT_BACK (-1)
-#define RIGHT_FRONT (-1)
+#define RIGHT_FORWARD (-1)
 #define RIGHT_BACK (1)
 
 Stepper stepperLeft(STEPS, 8, 10, 9, 11);
@@ -30,7 +30,12 @@ HCSR04 left_hcsr04(SR_LEFT_TRIG, SR_LEFT_ECHO, 20, 4000);
 HCSR04 right_hcsr04(SR_RIGHT_TRIG, SR_RIGHT_ECHO, 20, 4000);
 HCSR04 front_hcsr04(SR_FRONT_TRIG, SR_FRONT_ECHO, 20, 4000);
 
-int leftDist, rightDist, frontDist;
+//Timer for SR04
+#define SR_TIMER 500
+
+//Global variables
+unsigned long leftDist, rightDist, frontDist;
+unsigned long curTime;
 
 void setup(){
   //Serial initilization
@@ -39,24 +44,30 @@ void setup(){
   // set the speed of the motor to 30 RPMs
   stepperLeft.setSpeed(200);
   stepperRight.setSpeed(200);
+
+  //set current time
+  curTime = millis();
 }
 
-void loop() {
-
+void readUltrasonicData(void) {
   leftDist = left_hcsr04.distanceInMillimeters();
   rightDist = right_hcsr04.distanceInMillimeters();
   frontDist = front_hcsr04.distanceInMillimeters();
 
-  stepperRight.step(RIGHT_FRONT);
-  stepperLeft.step(LEFT_FRONT);
-
-//  while(frontDist < 100) {
-//    frontDist = front_hcsr04.distanceInMillimeters();
-//  }
-
-//     Output the distance in mm
+  //     Output the distance in mm
 //  Serial.print("left: " + String(leftDist));
 //  Serial.print("\tright :" + String(rightDist));
 //  Serial.println("\tfront :" + String(frontDist));
-//  delay(250);
+}
+
+void loop() {
+  if(millis() - curTime > SR_TIMER) {
+    readUltrasonicData();
+    curTime = millis();
+  }
+  if(frontDist > 100) {
+  stepperRight.step(RIGHT_FORWARD);
+  stepperLeft.step(LEFT_FORWARD);
+  }
+
 }
