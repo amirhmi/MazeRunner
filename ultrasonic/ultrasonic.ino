36e8dc6115d9ce30357016557356a85ac36d41cb
amirhmi <amirhmi1377@gmail.com>
2019-05-24 16:27:05 +0430

    revrted to HR04 library and checkwalls added

--- ultrasonic/ultrasonic.ino
+++ ultrasonic/ultrasonic.ino
@@ -1,9 +1,8 @@
-#include <NewPing.h>
+#include <hcsr04.h>
 #include <Stepper.h>
-#include <Time.h>
 
 // Stepper motor defenitions and constructor 
-#define STEPS 200
+#define STEPS (200)
 #define ONE_REV ((360 / 5.625) * 32)
 #define ONE_CM (ONE_REV / 18.2)
 #define WHEELS_DIST (16.7)
@@ -26,18 +25,21 @@ Stepper stepperRight(STEPS, 4, 6, 5, 7);
 #define SR_FRONT_TRIG 12
 #define SR_FRONT_ECHO 13
 
-#define MAX_PING_DIST 100
+#define MAX_PING_DIST (3500)
+#define MIN_PING_DIST (20)
+#define DEADEND_DIST (150)
 
-NewPing left_hcsr04(SR_LEFT_TRIG, SR_LEFT_ECHO, MAX_PING_DIST);
-NewPing right_hcsr04(SR_RIGHT_TRIG, SR_RIGHT_ECHO, MAX_PING_DIST);
-NewPing front_hcsr04(SR_FRONT_TRIG, SR_FRONT_ECHO, MAX_PING_DIST);
+HCSR04 left_hcsr04(SR_LEFT_TRIG, SR_LEFT_ECHO, MIN_PING_DIST, MAX_PING_DIST);
+HCSR04 right_hcsr04(SR_RIGHT_TRIG, SR_RIGHT_ECHO, MIN_PING_DIST, MAX_PING_DIST);
+HCSR04 front_hcsr04(SR_FRONT_TRIG, SR_FRONT_ECHO, MIN_PING_DIST, MAX_PING_DIST);
 
 //Timer for SR04
-#define SR_TIMER 500
+#define SR_TIMER (500)
 
 //Global variables
 unsigned long leftDist, rightDist, frontDist;
-unsigned long curTime;
+bool leftWall, rightWall, frontWall;
+int state;
 
 void setup(){
   //Serial initilization
@@ -46,31 +48,48 @@ void setup(){
   // set the speed of the motor to 30 RPMs
   stepperLeft.setSpeed(200);
   stepperRight.setSpeed(200);
-
-  //set current time
-  curTime = millis();
+  leftDist = rightDist = frontDist = MAX_PING_DIST;
+  leftWall = rightWall = frontWall = false;
 }
 
 void readUltrasonicData(void) {
-  leftDist = left_hcsr04.ping_cm();
-  rightDist = right_hcsr04.ping_cm();
-  frontDist = front_hcsr04.ping_cm();
-
-  //     Output the distance in mm
+  leftDist = left_hcsr04.distanceInMillimeters();
+  rightDist = right_hcsr04.distanceInMillimeters();
+  frontDist = front_hcsr04.distanceInMillimeters();
 //  Serial.print("left: " + String(leftDist));
 //  Serial.print("\tright :" + String(rightDist));
 //  Serial.println("\tfront :" + String(frontDist));
 }
 
-void loop() {
-//  if(millis() - curTime > SR_TIMER) {
-//    readUltrasonicData();
-//    curTime = millis();
-//  }
-  readUltrasonicData();
-  if(frontDist > 10) {
-  stepperRight.step(RIGHT_FORWARD * 10);
-  stepperLeft.step(LEFT_FORWARD * 10);
-  }
+void checkWalls()
+{
+  if (rightDist < DEADEND_DIST)
+    rightWall = true;
+  else
+    rightWall = false;
 
+  if (leftDist < DEADEND_DIST)
+    leftWall = true;
+  else
+    leftWall = false;
+
+  if (frontDist < DEADEND_DIST)
+    frontWall = true;
+  else
+    frontWall = false;
+
+  Serial.print("leftWall: " + String(leftWall));
+  Serial.print("\nrightWall: " + String(rightWall));
+  Serial.println("\nfrontWall: " + String(frontWall));
+  delay(500);
+}
+
+void checkState()
+{
+  
+}
+
+void loop() {
+  readUltrasonicData();
+  checkWalls();
 }
